dd06ab458f48a0349f02ae1ccaae5b583bad3204
Avidly <avidly@AvidlydeMac-mini.local>
2017-02-16 18:05:06 +0800

    Initial Commit

---
+++ AvidAnalysisDemoIos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AvidAnalysisDemoIos
+//
+//  Created by Avidly on 2017/2/16.
+//  Copyright © 2017年 Avidly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
